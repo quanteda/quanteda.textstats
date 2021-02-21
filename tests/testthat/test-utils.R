98f00be8aefcb1e16e63862ed838d7b92f3b1a4c
kbenoit <kbenoit@lse.ac.uk>
2021-02-21 11:04:43 +0000

    Make test quanteda version dependent

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -7,7 +7,11 @@ test_that("pattern2list is working with collocations", {
     toks <- quanteda::tokens(txt)
     type <- quanteda::types(toks)
     col <- textstat_collocations(toks, size = 2:3)
-    ids <- quanteda::object2id(col, type, 'fixed', TRUE)
+    ids <- if (packageVersion("quanteda") < 2.9) {
+        quanteda:::pattern2list(col, type, "fixed", TRUE)
+    } else {
+        quanteda::object2id(col, type, "fixed", TRUE)
+    }
     expect_equivalent(col$collocation,
                       vapply(ids, function(x, y) paste0(y[x], collapse = " "), character(1), type))
     expect_equal(names(ids), col$collocation)
