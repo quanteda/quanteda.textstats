a4f8180456f1c5616ab72f2646dca1af8f49d3b3
Kohei Watanabe <watanabe.kohei@gmail.com>
2023-10-26 14:26:45 +0900

    Delete unused function and move settings

--- src/collocations.cpp
+++ src/collocations.cpp
@@ -3,8 +3,13 @@
 #include <bitset>
 using namespace quanteda;
 
+#ifdef QUANTEDA
+float GLOBAL_PATTERN_MAX_LOAD_FACTOR = 0.05;
+float GLOBAL_NGRAMS_MAX_LOAD_FACTOR = 0.25;
+#endif
+
 #if QUANTEDA_USE_TBB
-typedef tbb::atomic<unsigned int> UintAtomic; // changed to std::atomic<unsigned int> for TBB 2021
+typedef tbb::atomic<unsigned int> UintAtomic; // NOTE: changed to std::atomic<unsigned int> for TBB 2021
 typedef tbb::concurrent_vector<std::pair<Ngram, UintAtomic>> VecPair;
 typedef tbb::concurrent_unordered_map<Ngram, std::pair<UintAtomic, UintAtomic>, hash_ngram, equal_ngram> MapNgramsPair;
 #else
