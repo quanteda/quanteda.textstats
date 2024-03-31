8fa8eba9711702f3eec1f14f15c47d555a9fff9d
Kohei Watanabe <watanabe.kohei@gmail.com>
2024-03-31 12:12:42 +0900

    Convert arma::mat to std::vector

--- src/keyness.cpp
+++ src/keyness.cpp
@@ -5,6 +5,14 @@ using namespace quanteda;
 
 static const double epsilon = 0.000000001; // the same value as R code
 
+inline std::vector<double> to_vector(const arma::sp_mat& mt) {
+    return arma::conv_to< std::vector<double> >::from(arma::mat(mt));
+}
+
+inline std::vector<double> to_vector(const arma::rowvec& v) {
+    return arma::conv_to< std::vector<double> >::from(v);
+}
+
 inline double yates_correction(
         const double &a,
         const double &b,
@@ -39,11 +47,11 @@ inline double williams_correction(
 inline double chisq_lambda(
         const double &a,
         const double &b,
-        const arma::colvec &mrg,
+        const std::vector<double> &mrg,
         const std::string &cor
 ) {
-    double tN = mrg(0);
-    double rN = mrg(1);
+    double tN = mrg[0];
+    double rN = mrg[1];
     double c = tN - a, d = rN - b, N = a + b + c + d, E = (a + b) * (a + c) / N;
     double delta = (cor == "default" || cor == "yates") ? yates_correction(a, b, c, d) : 0.0;
     double q = (cor == "williams") ? williams_correction(a, b, c, d) : 1.0;
@@ -55,11 +63,11 @@ inline double chisq_lambda(
 inline double lr_lambda(
         const double &a,
         const double &b,
-        const arma::colvec &mrg,
+        const std::vector<double> &mrg,
         const std::string &cor
 ) {
-    double tN = mrg(0);
-    double rN = mrg(1);
+    double tN = mrg[0];
+    double rN = mrg[1];
     double c = tN - a, d = rN - b, N = a + b + c + d, E = (a + b) * (a + c) / N;
     double aa = a, bb = b, cc = c , dd = d;
 
@@ -87,12 +95,12 @@ inline double lr_lambda(
 inline double pmi_lambda(
         const double &a,
         const double &b,
-        const arma::colvec &mrg,
+        const std::vector<double> &mrg,
         const bool normal = false
 ) {
 
-    const double tN = mrg(0);
-    const double rN = mrg(1);
+    const double tN = mrg[0];
+    const double rN = mrg[1];
     double c = tN - a, d = rN - b, N = a + b + c + d, E = (a + b) * (a + c) / N;
     double res = std::log(a / E + epsilon);
     if (normal)
@@ -110,25 +118,27 @@ Rcpp::NumericVector cpp_keyness(arma::sp_mat &mt,
     if (mt.n_rows != 2)
         throw std::range_error("Invalid DFM object");
 
-    arma::colvec margin(arma::sum(mt, 1));
-    DoubleParams stats(mt.n_cols);
-
     std::size_t I = mt.n_cols;
+    std::vector<double> margin = to_vector(arma::sum(mt, 1));
+    std::vector<double> row0 = to_vector(mt.row(0));
+    std::vector<double> row1 = to_vector(mt.row(1));
+    DoubleParams stats(mt.n_cols);
+    
 #if QUANTEDA_USE_TBB
     tbb::task_arena arena(thread);
     arena.execute([&]{
         tbb::parallel_for(tbb::blocked_range<int>(0, I), [&](tbb::blocked_range<int> r) {
             if (measure == "chi2") {
                 for (int i = r.begin(); i < r.end(); ++i) {
-                    stats[i] = chisq_lambda(mt(0, i), mt(1, i), margin, correct);
+                    stats[i] = chisq_lambda(row0[i], row1[i], margin, correct);
                 }
             } else if (measure == "lr") {
                 for (int i = r.begin(); i < r.end(); ++i) {
-                    stats[i] = lr_lambda(mt(0, i), mt(1, i), margin, correct);
+                    stats[i] = lr_lambda(row0[i], row1[i], margin, correct);
                 }
             } else if (measure == "pmi") {
                 for (int i = r.begin(); i < r.end(); ++i) {
-                    stats[i] = pmi_lambda(mt(0, i), mt(1, i), margin, false);
+                    stats[i] = pmi_lambda(row0[i], row1[i], margin, false);
                 }
             }
         });
@@ -136,15 +146,15 @@ Rcpp::NumericVector cpp_keyness(arma::sp_mat &mt,
 #else
     if (measure == "chi2") {
         for (std::size_t i = 0; i < I; i++) {
-            stats[i] = chisq_lambda(mt(0, i), mt(1, i), margin, correct);
+            stats[i] = chisq_lambda(row0[i], row1[i], margin, correct);
         }
     } else if (measure == "lr") {
         for (std::size_t i = 0; i < I; i++) {
-            stats[i] = lr_lambda(mt(0, i), mt(1, i), margin, correct);
+            stats[i] = lr_lambda(row0[i], row1[i], margin, correct);
         }
     } else if (measure == "pmi") {
         for (std::size_t i = 0; i < I; i++) {
-            stats[i] = pmi_lambda(mt(0, i), mt(1, i), margin, false);
+            stats[i] = pmi_lambda(row0[i], row1[i], margin, false);
         }
     }
 #endif
